d6bd58bdbb072f244e62478dc9fe32fec668f213
Alyssa Underwood <lyssaunderwood@fb.com>
2016-06-20 22:40:10 -0700

    First NYTimes

---
+++ app/src/main/java/com/example/lyssaunderwood/nytimessearch/Article.java
@@ -0,0 +1,61 @@
+package com.example.lyssaunderwood.nytimessearch;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Article implements Serializable{
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public String getThumbNail() {
+        return thumbNail;
+    }
+
+    String webUrl;
+    String headline;
+    String thumbNail;
+
+    public Article(JSONObject jsonObject) {
+        try {
+            this.webUrl = jsonObject.getString("web_url");
+            this.headline = jsonObject.getJSONObject("headline").getString("main");
+
+            JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+
+            if (multimedia.length() > 0) {
+                JSONObject multimediaJson = multimedia.getJSONObject(0);
+                this.thumbNail = "http://www.nytimes.com/" + multimediaJson.getString("url");
+            } else {
+                this.thumbNail = "";
+            }
+        } catch (JSONException e){
+
+        }
+    }
+
+    public static ArrayList<Article> fromJSONArray (JSONArray array) {
+        ArrayList<Article> results = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++)
+        {
+            try {
+                results.add(new Article(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
---
+++ app/src/main/java/com/example/lyssaunderwood/nytimessearch/ArticleAdapter.java
@@ -0,0 +1,103 @@
+package com.example.lyssaunderwood.nytimessearch;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lyssaunderwood.nytimessearch.activities.ArticleActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+public class ArticleAdapter extends
+        RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView tvTitle;
+        public ImageView imageView;
+        private Context context;
+
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            imageView = (ImageView) itemView.findViewById(R.id.ivImage);
+            //this.context = context;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition();
+            Article article = mArticles.get(position);
+            //Toast.makeText(v.getContext(), "Clicked", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(v.getContext(), ArticleActivity.class);
+            i.putExtra("article", article);
+            v.getContext().startActivity(i);
+
+        }
+    }
+
+    private ArrayList<Article> mArticles;
+
+    public ArticleAdapter(ArrayList<Article> articles) {
+        mArticles = articles;
+    }
+
+    @Override
+    public ArticleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View articleView = inflater.inflate(R.layout.item_article_result, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(articleView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(ArticleAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Article article = mArticles.get(position);
+
+        // Set item views based on the data model
+        TextView textView = viewHolder.tvTitle;
+        textView.setText(article.getHeadline());
+
+        ImageView imageView = viewHolder.imageView;
+        imageView.setImageResource(0);
+        String thumbnail = article.getThumbNail();
+        if (!TextUtils.isEmpty(thumbnail)) {
+            Picasso.with(imageView.getContext()).load(thumbnail).into(imageView);
+        }
+
+
+    }
+
+    // Return the total count of items
+    @Override
+    public int getItemCount() {
+        return mArticles.size();
+    }
+}
